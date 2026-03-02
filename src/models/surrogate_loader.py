55a38946569c10fca29e86d763655bfd024c4d58
Balaji S B <balajibavimane@gmail.com>
2026-03-02 19:30:26 +0100

    model wrapper to handle huyvnphan's pretrained models

---
+++ src/models/surrogate_loader.py
@@ -0,0 +1,3 @@
+# load surrogate non robust models from pretrained checkpoints
+# https://github.com/huyvnphan/PyTorch_CIFAR10
+# first we need to download and extrach state_dicts from https://drive.google.com/file/d/17fmN8eQdLpq2jIMQ_X0IXDPXfI9oVWgq/view?usp=sharing
