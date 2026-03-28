bd8ff1f59ec6970e799944e901ad1a1b16f8502b
Balaji S B <balajibavimane@gmail.com>
2026-03-28 23:16:06 +0100

    update metadata

--- src/metadata.py
+++ src/metadata.py
@@ -52,7 +52,63 @@ model_metadata: dict[str, dict] = {
     "Bartoldson2024Adversarial_WRN-94-16": {
         "reported_clean_acc": 93.68,
         "reported_robust_acc": 73.71,
-    }
+    },
+    "Gowal2021Improving_28_10_ddpm_100m": {
+        "reported_clean_acc": 87.50,
+        "reported_robust_acc": 63.38,
+    },
+    "Sehwag2021Proxy_R18": {
+        "reported_clean_acc": 84.59,
+        "reported_robust_acc": 55.54,
+    },
+    "Addepalli2022Efficient_RN18": {
+        "reported_clean_acc": 86.04,
+        "reported_robust_acc": 51.56,
+    },
+    "Xu2023Exploring_WRN-28-10": {
+        "reported_clean_acc": 93.69,
+        "reported_robust_acc": 63.89,
+    },
+    "Rice2020Overfitting": {
+        "reported_clean_acc": 85.34,
+        "reported_robust_acc": 53.42,
+    },
+    "Wong2020Fast": {
+        "reported_clean_acc": 83.34,
+        "reported_robust_acc": 43.21,
+    },
+    "Andriushchenko2020Understanding": {
+        "reported_clean_acc": 79.84,
+        "reported_robust_acc": 43.93,
+    },
+    "Ding2020MMA": {
+        "reported_clean_acc": 84.36,
+        "reported_robust_acc": 41.44,
+    },
+    "Engstrom2019Robustness": {
+        "reported_clean_acc": 87.03,
+        "reported_robust_acc": 49.25,
+    },
+    "Zhang2019You": {
+        "reported_clean_acc": 87.20,
+        "reported_robust_acc": 44.83,
+    },
+    "Sitawarin2020Improving": {
+        "reported_clean_acc": 86.84,
+        "reported_robust_acc": 43.21,
+    },
+    "Huang2020Self" : {
+        "reported_clean_acc": 83.48,
+        "reported_robust_acc": 53.34,
+    },
+    "Chen2020Efficient" : {
+        "reported_clean_acc": 85.32,
+        "reported_robust_acc": 51.12,
+    },
+    "Chen2020Adversarial" : {
+        "reported_clean_acc": 86.04,
+        "reported_robust_acc": 51.56,
+    },
 }
 
 
